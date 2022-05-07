03f2ba6bbf657a2bc69b2d221ee7325dae9ba0f2
vicsopin <victor.sopin@student.lut.fi>
2022-05-07 07:31:50 +0300

    added main for UNO

---
+++ UNO/main.c
@@ -0,0 +1,37 @@
+/*
+ PIR Motion Sensor Interface with Uno
+ */
+
+#define F_CPU 16000000UL
+#include <avr/io.h>
+#include <util/delay.h>
+
+int main(void)
+{
+    DDRB = 0b00000000; // inputs
+    PORTB = 0b00000001; // enable only first pin
+    DDRD = 0b00001100; // outputs
+    
+    int i;
+
+    while(1)
+    {
+        if(PINB & (1<<PB0)) {
+            
+            PORTD |= (1<<PD3);
+            
+            // I have a passive buzzer so need to oscillate
+            for (i=0; i < 10; ++i)
+            {
+                PORTD |= (1<<PD2);
+                _delay_ms(1);
+                PORTD &= ~(1<<PD2);
+                _delay_ms(1);
+            }
+
+        }
+        else
+            PORTD &= ~(1<<PD2);
+            PORTD &= ~(1<<PD3);
+	} return 0;
+}
