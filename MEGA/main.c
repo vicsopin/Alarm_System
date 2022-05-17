de14c03a1d5989e7ec303bc06b65160546322bed
Victor Sopin <59625852+vicsopin@users.noreply.github.com>
2022-05-17 21:59:57 +0300

    Update main.c

--- MEGA/main.c
+++ MEGA/main.c
@@ -1,176 +1,262 @@
 #define F_CPU 16000000UL
+#define FOSC 16000000UL // Clock Speed
+#define BAUD 9600
+#define MYUBRR (FOSC/16/BAUD-1)
+#define FUNC_EXEC_TIME 5
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdbool.h>
 #include <string.h>
 #include <avr/io.h>
 #include <util/delay.h>
-#include <avr/array.h>
+#include <avr/eeprom.h>
+#include <time.h>
 #include "lcd.h"
 #include "keypad.h"
 
-uint8_t key_1, key_2, key_3, key_4;
-uint8_t key_arm = '0';
+//uint8_t word[256];
+char str;
+//unsigned long int code;
+char inputArray[69];
+char code[69] = "6969";
+
+// system variables
+bool armed = true;
+bool buzzer = false;
+bool timer = false;
+//clock_t clock(void);
 
 
-int c;
-int i;
-int password[50]; 
-
-int get_password();
-void check_password();
-//void system_arm(); // function that initiates arm procedures
-
-///////////// Menu items ///////////////
-
-// check password[i( 3 attempts)
-int get_password() {
-
-	for ( c = 0; c < 3; c++ ) // check password[ifunction
-	{
-		lcd_clrscr();
-		lcd_puts("Enter password:");
-
-		for ( i = 0; i++ ){ 
-
-			while(1) {
-				lcd_gotoxy(0,i);
-
-				switch(KEYPAD_GetKey()){
-					case '1':
-						password[i] = '1';
-						i++;
-					case '2':
-						password[i] = '2';
-						i++;
-					case '3':
-						password[i] = '3';
-						i++;
-					case '4':
-						password[i] = '4';
-						i++;
-					case '5':
-						password[i] = '5';
-						i++;
-					case '6':
-						password[i] = '6';
-						i++;
-					case '7':
-						password[i] = '7';
-						i++;
-					case '8':
-						password[i] = '8';
-						i++;
-					case '9':
-						password[i] = '9';
-						i++;
-					case '0':
-						password[i] = '0';
-						i++;
-					case '/':
-						password[-1] = password[i+1];
-						i--;
-						lcd_gotoxy(0,i);
-						lcd_putc('');
-					case '*':
-						if check_password {
-							c==3;
-
-						}
-						return password;
-					default:
-						continue;
-				}
-
-
-		}
-		// _delay_ms(1000);
-		// lcd_clrscr();
-		// lcd_puts("Enter password[i");
-		// lcd_gotoxy(0,1);
-		// key_1 = KEYPAD_GetKey();
-		// if(0 <= key_1 <= 9) {
-		// 	continue
-		// }
-		// else if(key_1 == '*')
-		// lcd_putc(key_1);
-		// lcd_gotoxy(1,2);
-		// key_2 = KEYPAD_GetKey();
-		// lcd_putc(key_2);
-		// lcd_gotoxy(2,3);
-		// key_3 = KEYPAD_GetKey();
-		// lcd_putc(key_3);
-		// lcd_gotoxy(3,4);
-		// key_4 = KEYPAD_GetKey();
-		// lcd_putc(key_4);
-		
-		// _delay_ms(1000);
-		
-		// if(key_1=='6' && key_2=='9' && key_3=='6' && key_4=='9'){
-		// 	lcd_gotoxy(0,0);
-		// 	lcd_clrscr();
-		// 	lcd_puts("Correct password[i);
-		// 	break;
-		// }
-		
-		// else {
-		// 	lcd_gotoxy(0,0);
-		// 	lcd_clrscr();
-		// 	lcd_puts("Wrong password[i);
-		// 	continue;
-		// }
-		
-	}
-}
-
-void check_password(password) {
-
-	if (sizeof(password) == 4 ){
-
-	}
-
-	else {
-
-	}
-
-}
-
-///////////// Menu items end ///////////////
-
-//void system_arm()
-int main()
+static void USART_init(uint16_t ubrr) // unsigned int
 {
-	// initialize display, cursor off
+	/* Set baud rate in the USART Baud Rate Registers (UBRR) */
+	UBRR0H = (unsigned char) (ubrr >> 8);
+	UBRR0L = (unsigned char) ubrr;
+	
+	/* Enable receiver and transmitter on RX0 and TX0 */
+	UCSR0B |= (1 << RXEN0) | (1 << TXEN0); //NOTE: the ATmega328p has 1 UART: 0
+	// UCSR0B |= (1 << 4) | (1 << 3);
+	
+	/* Set frame format: 8 bit data, 2 stop bit */
+	UCSR0C |= (1 << USBS0) | (3 << UCSZ00);
+	// UCSR0C |= (1 << 3) | (3 << 1);
+	
+}
+static void USART_Transmit(unsigned char data, FILE *stream)
+{
+	/* Wait until the transmit buffer is empty*/
+	while(!(UCSR0A & (1 << UDRE0)))
+	{
+		;
+	}
+	
+	/* Put the data into a buffer, then send/transmit the data */
+	UDR0 = data;
+}
+static char USART_Receive(FILE *stream)
+{
+	/* Wait until the transmit buffer is empty*/
+	while(!(UCSR0A & (1 << UDRE0)))
+	{
+		;
+	}
+	
+	/* Get the received data from the buffer */
+	return UDR0;
+}
+
+FILE uart_output = FDEV_SETUP_STREAM(USART_Transmit, NULL, _FDEV_SETUP_WRITE);
+FILE uart_input = FDEV_SETUP_STREAM(NULL, USART_Receive, _FDEV_SETUP_READ);
+
+//void timeout(double seconds, FN_PNTR) //Where FN_PNTR is a real function pointer
+//{
+	//time_t start = clock();
+	//while ((clock() - start) < seconds * CLOCKS_PER_SEC)
+	//{
+		//if ( ! FN_PNTR) /* if the operation fails based on the return value*/
+		//continue;
+		//else
+		//break; /* it worked */
+	//}
+	//exit(0); /* timed out... leave the program */
+//}
+
+bool checkPassword() {
+	
+	//this should be asynchronous, meaning that other code should continue when this executes
+	if(clock() > FUNC_EXEC_TIME * 1000) return FALSE;
+	
+	get_input();
+	if(strcmp(inputArray,code) == 0) {
+		lcd_clrscr();
+		lcd_gotoxy(0, 0);
+		lcd_puts("Correct password");
+		printf("Correct password\n");
+		return TRUE;
+	}
+	else{
+		lcd_clrscr();
+		lcd_gotoxy(0, 0);
+		lcd_puts("Incorrect password");
+		printf("Incorrect password\n");
+		return FALSE;
+	}
+}
+
+void get_input(){
+	int i = 0;
+	//empty array always when asking for input
+	memset(inputArray,0,69);
+	
+	while(1) {
+		str = KEYPAD_GetKey();
+		if(str) {
+			if (str == 'A' || str == 'B' || str == 'C' || str == 'D') continue;
+
+			if (i >= 4 && str == '#')
+			{
+				printf("[INPUT]	%s\n",inputArray);
+				break;
+			}
+			if (str=='*' && i>0)
+			{
+				inputArray[i-1] = NULL;
+				i--;
+			}
+			else if (str=='*' && i==0){
+				continue;
+			}
+			else {
+				inputArray[i] = str;
+				i++;
+			}
+			printf("Input value: %c, i=%d\n", str, i);
+		}
+	}
+}
+
+void lcd_state_armed() {
+	printf("[LCD]	system armed\n");
+	lcd_clrscr();
+	lcd_gotoxy(0,0);
+	lcd_puts("System Armed");
+	lcd_gotoxy(0,1);
+	lcd_puts("Press D to disarm");
+	
+}
+
+void lcd_state_disarmed() {
+	printf("[LCD]	system disarmed\n");
+	lcd_clrscr();
+	lcd_gotoxy(0,0);
+	lcd_puts("System Disarmed");
+	lcd_gotoxy(0,1);
+	lcd_puts("Press A to arm");
+}
+
+void lcd_ask_password() {
+	printf("[LCD]	asking for password\n");
+	lcd_clrscr();
+	lcd_gotoxy(0,0);
+	lcd_puts("Enter password:");
+	lcd_gotoxy(0,1);
+}
+
+void lcd_set_password() {
+	printf("[LCD]	setting new password\n");
+	lcd_clrscr();
+	lcd_gotoxy(0,0);
+	lcd_puts("Enter old password:");
+	lcd_gotoxy(0,1);
+}
+
+void lcd_print_help() {
+	printf("[LCD]	printing help\n");
+	lcd_clrscr();
+	lcd_gotoxy(0,0);
+	lcd_puts("Enter new password:");
+	lcd_gotoxy(0,1);
+}
+
+void lcd_print_status() {
+	printf("[LCD]	SYSTEM IS %i\n",armed);
+	lcd_clrscr();
+	lcd_gotoxy(0,0);
+	if(armed) {
+		lcd_puts("System is armed");
+	} else {
+		lcd_puts("System is disarmed");
+	}
+	lcd_gotoxy(0,1);
+}
+
+void lcd_password_changed() {
+	printf("[LCD]	password changed\n");
+	lcd_clrscr();
+	lcd_gotoxy(0,0);
+	lcd_puts("Password changed");
+}
+
+
+int main(void) {
+	
+	USART_init(MYUBRR);
+	stdout = &uart_output;
+	stdin = &uart_input;
 	lcd_init(LCD_DISP_ON);
 	KEYPAD_Init();
-	while(1) {
-		// main menu function here
-		lcd_gotoxy(0,0);
-		lcd_clrscr();
-		lcd_puts("System disarmed");
-		lcd_gotoxy(0,1);
-		lcd_puts("To arm press 1");
-		key_arm = KEYPAD_GetKey();
-
-		while(key_arm=='1'){
+	
+	while(1){
+		// show system status at the start of the loop
+		lcd_print_status();
 		
-			check_password();
-			//system_arm();
-			_delay_ms(1000);
-			lcd_gotoxy(0,0);
-			lcd_clrscr();
-			lcd_puts("System is armed");
-			lcd_gotoxy(0,1);
-			lcd_puts("Press 0 to disarm");
-			key_arm = KEYPAD_GetKey();
+		// get input from the user
+		char input = KEYPAD_GetKey();
+		printf("Input %c\n",input);
 		
-			if(key_arm=='0') {
-				check_password();
-				break;
+		
+		switch(input)
+		{
+		case 'A': //arm the system
+			if(!armed) {
+				lcd_ask_password();
+				if (checkPassword()) {
+					lcd_state_armed();
+					armed = true;
 				}
+				break;
+			} else {
+				printf("[LCD]	cannot arm an armed system\n");
+				lcd_clrscr();
+				lcd_gotoxy(0,0);
+				lcd_puts("Already armed");
+				_delay_ms(1000);
+			}
+						
+		case 'C': //change or set the password
+			lcd_set_password();
+			if (checkPassword()) {
+				lcd_clrscr();
+				lcd_gotoxy(0,0);
+				lcd_puts("New password");
+				get_input();
+				
+				lcd_password_changed();
+				strcpy(code, inputArray);
+			}
+			break;
+			
+		case 'D': //disarm the system
+			lcd_ask_password();
+			if (checkPassword()) {
+				lcd_state_disarmed();
+				armed = false;
+			}
+			break;
+		default:
+			lcd_print_help();
 		}
-
-	}
-
-	return 0;
+			
+	}	
 }
